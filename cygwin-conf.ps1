85624b84e98a1bc6b28fcdb4958245125e1cc89c
Guy Lafleur <kevin.netherton@gov.bc.ca>
2021-03-12 13:45:39 -0800

    Difficulty downloading action artifacts * trying to create a release artifact instead

--- cygwin-conf.ps1
+++ cygwin-conf.ps1
@@ -6,6 +6,12 @@ $cygSetup = Join-Path -Path $cygpackages -ChildPath setup_cygwin.exe
 $wgrib2Dir = Join-Path -Path $curDir -ChildPath wgrib-cyg
 $aptCyg = Join-Path -Path $cygdirbin -ChildPath apt-cyg
 
+# delete for debugging
+Remove-Item $cygdir -Recurse -Force -Confirm:$false
+Remove-Item $cygpackages -Recurse -Force -Confirm:$false
+Remove-Item ./setup.log.full -Recurse -Force -Confirm:$false
+
+
 New-Item -ItemType Directory -Force -Path $cygdir
 New-Item -ItemType Directory -Force -Path $cygpackages
 New-Item -ItemType Directory -Force -Path $wgrib2Dir
@@ -15,32 +21,27 @@ wget http://cygwin.com/setup-x86_64.exe -o $cygSetup
 # not finished here... may need to install other cygwin deps, need tof igure out how to do that
 #http://mirror.csclub.uwaterloo.ca
 #http://muug.ca/mirror/cygwin/
+#http://cygwin.mirrors.pair.com/ 
 
 #--quiet-mode `
 # --local-install `
+# --local-package-dir $cygpackages `
 
+# cannot split package list onto multiple lines, or they don't get installed.
 & $cygSetup `
---no-admin `
---local-package-dir $cygpackages `
---site http://cygwin.mirrors.pair.com/ `
---root $cygdir `
---verbose `
 --quiet-mode `
+--no-desktop `
+--download `
+--local-install `
+--site http://muug.ca/mirror/cygwin/ `
+--local-package-dir $cygpackages `
+--root $cygdir `
+--no-admin `
 --no-startmenu `
 --disable-buggy-antivirus `
---packages `
-awk, `
-make, `
-gcc-core, `
-gcc-fortran, `
-gcc-g++, `
-make, `
-unzip, `
-wget, `
-curl, `
-nano, `
-cygport, `
-zlib
+--packages awk,unzip,lynx,wget,curl,nano,gzip,bzip,cygport,libgomp1,zlib,libgfortran5
+-C base `
+
 
 
 echo cygwin install complete packages installed
@@ -50,6 +51,10 @@ echo cygwin install complete packages installed
 
 #$env:path = "$cygdirbin;$env:path"
 #& $cygdirbin\bash.exe -c "./build-wgrib2.sh"
+
+
+# test wgrib
+& $cygdirbin\bash.exe -c "./wgrib-cyg/grib2/wgrib2/wgrib2.exe"
 
 # cd $wgrib2Dir
 # $env:CC = "gcc"
